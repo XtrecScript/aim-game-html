1aa836b740df3cfa774ab7a75815c1b72c374f2f
XtrecScript <70416821+XtrecScript@users.noreply.github.com>
2021-11-03 13:36:39 -0500

    v1

---
+++ script.js
@@ -0,0 +1,55 @@
+document.getElementById("player").addEventListener("mouseover", sumarPuntos);
+document.getElementById("enemigo").addEventListener("mouseover", perderVida);
+puntos = 0;
+tiempo = 20;
+nesecarios = 10;
+nivel = 1
+
+document.getElementById("player").addEventListener("mouseover", sumarPuntos);
+document.getElementById("enemigo").addEventListener("mouseover", perderVida);
+puntos = 0;
+tiempo = 20;
+nesecarios = 10;
+
+window.onload = function enemigo() {
+    ranNum = Math.round(Math.random() * 500);
+    ranNum2 = Math.round(Math.random() * 500);
+    document.getElementById("enemigo").style.marginTop = ranNum + "px";
+    document.getElementById("enemigo").style.marginLeft = ranNum2 + "px";
+    setInterval(enemigo, 1000);
+}
+
+function perderVida() {
+    alert("Perdiste, no toques a las bombas")
+    puntos = 0;
+    tiempo = 20;
+    location.href = location.href
+}
+
+function sumarPuntos() {
+    puntos++;
+    document.getElementById("puntos").innerHTML = "🔫&nbsp;Puntos: " + puntos + "/" + nesecarios;
+    ranNum = Math.round(Math.random() * 500);
+    ranNum2 = Math.round(Math.random() * 500);
+    document.getElementById("player").style.marginTop = ranNum + "px";
+    document.getElementById("player").style.marginLeft = ranNum2 + "px";
+
+    if (puntos == 10) {
+        location.href = "nivel2.html"
+        tiempo = 20;
+        puntos = 0;
+        nivel = nivel + 1
+    }
+}
+
+function restarTiempo() {
+    tiempo--
+    document.getElementById("tiempo").innerHTML = "&nbsp;&nbsp;&nbsp;⏰&nbspTiempo: " + tiempo;
+    if (tiempo == 0) {
+        alert("Perdiste :(")
+        tiempo = 20;
+        puntos = 0;
+        location.href = location.href
+    }
+}
+setInterval(restarTiempo, 1000);
